46a4cc62c3e08de247d1ab394b14fa67dc1da001
prashantkumar182000 <prashantkumar18.pk@gmail.com>
2024-10-05 19:49:31 +0530

    @updation6.0

--- src/pages/api/testBinance.js
+++ src/pages/api/testBinance.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart', {
-        params: {
-          vs_currency: 'usd',
-          days: req.query.interval || '1', // 1 day, 7 days, etc.
+    const response = await axios.get('https://api.binance.com/api/v3/klines', {
+      params: {
+        symbol: 'BTCUSDT',
+        interval: '1m',
+        limit: 1, // Fetch a small sample
       },
     });
     res.status(200).json({ message: 'Binance API works!', data: response.data });
